6ec2f04684735c686dff23676a97aee85fbc3d01
Ben Jammaa KHALIL <khalil.benjemmaa@esprit.tn>
2025-08-15 02:24:01 +0100

    Add GitHub Actions workflow for deployment

--- resume-portfolio/vite.config.js
+++ resume-portfolio/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this base property for GitHub Pages deployment
+  base: '/resume-portfolio/', 
 })
